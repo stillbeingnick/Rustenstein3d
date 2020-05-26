cd2e55e385f9a69a604ad0e408ad29d5a0238b15
Nicholas Still <nicholas.still@jda.com>
2020-05-25 20:57:54 -0700

    playing with frame buffers

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,117 @@
 use std::f32::consts::PI;
+use minifb::{Key, Window, WindowOptions, ScaleMode, Scale};
+use std::time::SystemTime;
+
+const D_WIDTH: usize = 100;
+const D_HEIGHT: usize = 100;
 
 fn main() {
 
-    let mut fake_display = vec![
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    ];
+    let mut buffer: Vec<u32> = vec![0; D_WIDTH * D_HEIGHT];
+
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        D_WIDTH,
+        D_HEIGHT,
+        WindowOptions{
+            resize: true,
+            scale: Scale::X8,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // let mut my_buff: Vec<u32> = vec![0; D_WIDTH * D_HEIGHT];
+
+    
+
+
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        //loop for the window
+        let frame_time = SystemTime::from_inner(SystemTime::now());
+        for xi in 0..D_WIDTH * D_HEIGHT{
+            if frame_time % 2 == 0 {
+                if xi % 2 == 0 {
+                    buffer[xi] = 100;
+                }else{
+                    buffer[xi] = 255;
+                }
+            }else{
+                if xi % 2 == 0 {
+                    buffer[xi] = 255;
+                }else{
+                    buffer[xi] = 100;
+                }
+            }
+        }
+
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, D_WIDTH, D_HEIGHT)
+            .unwrap();
+    }
+
+    
+
+    let mut map: Vec<Vec<&str>> = vec![vec![]];
+    map.push("##########".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("#........#".split("").collect());
+    map.push("##########".split("").collect());
+    
+    // let mut fake_display = vec![
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    //     vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    // ];
+    // let mut display_buffer: Vec<Vec<&str>> = vec![vec![]];
+    // //create fake display buffer
+    // for y in 0..d_height{
+    //     let mut cols: Vec<&str> = vec![];
+    //     for x in 0..d_width{
+    //         cols.push("#");
+    //     }
+    //     display_buffer.push(cols);
+
+    // }
+    // //testing the display buffer
+    // for yx in 0..display_buffer.len(){
+    //     println!("{:?}", display_buffer[yx]);
+    // }
+
+
 
     //fov entire cone of sight diveded in half to make the calcs easier given the right angle in the center
     //hard code fov at 90 right now
@@ -71,7 +157,9 @@ fn main() {
 
     let mut rays = vec![];
 
+    // fake_display[player.y as usize][player.x as usize] = 1;
 
+    //init the rays
     for x in 0..ray_count as i32 {
         let rad_dev = ray_count/fov_rads;
         let current_rad;
@@ -90,142 +178,31 @@ fn main() {
         }else{
             ray.dirX = ray.dirY*(current_rad.tan()*(PI/180.0));
         }
-        // let round_x = (player.x + ray.dirX).round();
-        // let round_y = (player.x + ray.dirY).round();
-
-        // println!("x: {} y: {}",round_x, round_y);
-
-        // if round_x >= 0.0 && round_y >= 0.0 && round_x <= 9.0 && round_y <= 9.0 {
-        //     fake_display[round_x as usize][round_y as usize] = 1;
-            
-        // }
-
-        // println!("{}", round_x as usize);
-        // fake_display[(ray.dirX.round()) as usize][(ray.dirY.round()) as usize] = 1;
         rays.push(ray);
     }
-    for j in 0..10 {
-        player.x += 1.0;
+
+    //testing the ray casting
+    // for j in 0..10 {
+    //     player.x += 1.0;
     
-        for x in 0..rays.len() {
-            let ray = &rays[x];
-            let mut current_point = [player.x, player.y];
-            for i in 0..500{
-                current_point = [current_point[0] + ray.dirX, current_point[1] + ray.dirY];
-                if current_point[0] <= 20.0 && current_point[1] <= 20.0 && current_point[0] > 0.0 && current_point[1] > 0.0 {
-                    // println!("{:?}",current_point);
-                    fake_display[current_point[1] as usize][current_point[0] as usize] = 1;
-                }else{
-                    break;
-                }
+    //     for x in 0..rays.len() {
+    //         let ray = &rays[x];
+    //         let mut current_point = [player.x, player.y];
+    //         for i in 0..500{
+    //             current_point = [current_point[0] + ray.dirX, current_point[1] + ray.dirY];
+    //             if current_point[0] <= 20.0 && current_point[1] <= 20.0 && current_point[0] > 0.0 && current_point[1] > 0.0 {
+    //                 // println!("{:?}",current_point);
+    //                 // fake_display[current_point[1] as usize][current_point[0] as usize] = 1;
+    //             }else{
+    //                 break;
+    //             }
 
-            }
-        }
-        for x in 0..fake_display.len() {
-            println!("{:?}", fake_display[x as usize])
-        }
-    }
-
-
-    // let l_fov = Ray{
-    //     start: PosVec {
-    //         x: player.x,
-    //         y: player.y
-    //     },
-    //     end: PosVec { 
-    //         x : player.x - ((fov*0.5)*(PI/180.0).tan())*ray_length, 
-    //         y: player.y + ray_length}
-    // };
-
-    // let r_fov = Ray {
-    //     start: PosVec {
-    //         x: player.x,
-    //         y: player.y 
-    //     },
-    //     end: PosVec {
-    //         x: player.x + ((fov*0.5)*(PI/180.0).tan())*ray_length,
-    //         y: player.y + ray_length
-    //     }
-    // };
-
-    // let ray_difference = PosVec {
-    //     x: l_fov.end.x - r_fov.end.x,
-    //     y: l_fov.end.y - r_fov.end.y
-    // }
-
-    
-    // for x in 0..ray_count as i32 {
-    //     if ray_difference.x != 0.0 {
-    //         let ray_x_factor = ray_difference.x/ray_count;
-    //     }else{
-    //         let ray_x_factor = 0.0;
-
-    //     }
-    //     if ray_difference.y != 0.0 {
-    //         let ray_y_factor = ray_difference.y/ray_count;
-    //     }else{
-    //         let ray_y_factor = 0.0;
-
-    //     }
-    //     let new_ray = Ray {
-    //         start: PosVec {
-    //             x: player.x,
-    //             y: player.y
-    //         },
-    //         end: PosVec {
-    //             x: 0.0,
-    //             y: 0.0
     //         }
     //     }
-    //     if rays.len() > 1 {
-    //         let previous_ray = &rays[x-1];
-    //         if(ray_x_factor !=)
-
-    //     }else{
-            
+    //     for x in 0..fake_display.len() {
+    //         // println!("{:?}", fake_display[x as usize])
     //     }
     // }
-
-
-    // let middle_ray = Ray {
-    //     start: PosVec {
-    //         x: player.x,
-    //         y: player.y
-    //     },
-    //     end: PosVec {
-    //         x: player.x + 10.0,
-    //         y: player.y + 10.0
-    //     }
-    // };
-
-    // rays.push(middle_ray);
-
-    fake_display[player.y as usize][player.x as usize] = 1;
-
-
-    let mut map: Vec<Vec<&str>> = vec![vec![]];
-    map.push("##########".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("#........#".split("").collect());
-    map.push("##########".split("").collect());
-
-    // for x in 0..fake_display.len() {
-    //     println!("{:?}", fake_display[x as usize])
-    // }
-
-    // print!("{:?}{:?}", l_fov, r_fov);
-    // print!("{:?}", rays);
-    // print!("{}", fov_rads/ray_count)
-
-    // loop{
-
-    // }
-
 
 
 }
