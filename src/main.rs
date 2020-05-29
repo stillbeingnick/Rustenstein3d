926e2239cd8e39bd062fc454bc1b86ff9e8e3671
stillbeingnick <nickstill93@gmail.com>
2020-05-29 09:02:04 -0700

    I think I might be losing it

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::f32::consts::PI;
 use minifb::{Key, Window, WindowOptions, ScaleMode, Scale};
 use std::time::SystemTime;
 
-const D_WIDTH: usize = 100;
-const D_HEIGHT: usize = 100;
+const D_WIDTH: usize = 640;
+const D_HEIGHT: usize = 640;
 
 fn main() {
 
@@ -15,8 +15,8 @@ fn main() {
     vec!["#",".",".",".",".",".",".",".",".","#"],
     vec!["#",".",".",".",".",".",".",".",".","#"],
     vec!["#",".",".",".",".",".",".",".",".","#"],
-    vec!["#",".",".",".",".",".",".",".",".","#"],
-    vec!["#",".",".",".",".",".",".",".",".","#"],
+    vec!["#",".",".",".",".","#",".",".",".","#"],
+    vec!["#",".",".","#",".",".",".",".",".","#"],
     vec!["#",".",".",".",".",".",".",".",".","#"],
     vec!["#",".",".",".",".",".",".",".",".","#"],
     vec!["#",".",".",".",".",".",".",".",".","#"],
@@ -27,9 +27,14 @@ fn main() {
     let fov: f32 = 90.0;
 
     #[derive(Debug, Copy, Clone)]
-    struct PosVec {
+    struct FloatVec {
         x: f32,
         y: f32
+    }
+    #[derive(Debug, Copy, Clone)]
+    struct IntVec {
+        x: i32,
+        y: i32
     }
     #[derive(Debug)]
     struct Ray {
@@ -40,19 +45,19 @@ fn main() {
     struct Player {
         x: f32,
         y: f32,
-        dir: PosVec
+        dir: FloatVec
     }
 
     let mut player = Player{
-        x: 5.0,
-        y: 5.0,
-        dir: PosVec{
-            x: 1.0,
+        x: 2.0,
+        y: 2.0,
+        dir: FloatVec{
+            x: -1.0,
             y: 0.0
         }
     };
 
-    let mut plane = PosVec {
+    let mut plane = FloatVec {
         x: 0.0,
         y: 0.66
     };
@@ -64,7 +69,7 @@ fn main() {
         D_HEIGHT,
         WindowOptions{
             resize: true,
-            scale: Scale::X8,
+            scale: Scale::X2,
             ..WindowOptions::default()
         },
     )
@@ -84,27 +89,34 @@ fn main() {
     let mut frame_time = 0.0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        // println!("{:?}", player);
 
         let frame_start = SystemTime::now();
 
         buffer = vec![0; D_WIDTH * D_HEIGHT];
 
-        let rot_speed: f32 = -1.0*frame_time;
-        let move_speed: f32 = 0.10*frame_time;
+        let rot_speed: f32 = frame_time*-0.5;
+        let move_speed: f32 = frame_time*0.10;
+
+        // println!("{}", move_speed);
 
         if window.is_key_down(Key::W) {
-            if map[(player.y + player.dir.y*move_speed) as usize][player.x as usize] != "" {player.y += player.dir.y*move_speed};
-            if map[player.y as usize][(player.x + player.dir.x*move_speed) as usize] != "" {player.x += player.dir.x*move_speed};
+            if ((player.y + player.dir.y*move_speed) as usize) < map.len()
+                && (player.y as usize) < map.len(){
+            if map[(player.y + player.dir.y*move_speed) as usize].get(player.x as usize) != None {player.y += player.dir.y*move_speed};
+            if map[player.y as usize].get((player.x + player.dir.x*move_speed) as usize) != None {player.x += player.dir.x*move_speed};
+            }
         }
         if window.is_key_down(Key::S) && player.y > 0.0 {
-            if map[(player.y - player.dir.y*move_speed) as usize][player.x as usize] != "" {player.y -= player.dir.y*move_speed};
-            if map[player.y as usize][(player.y - player.dir.y*move_speed) as usize] != "" {player.x -= player.dir.x*move_speed};
+            // println!("{}",map[(player.y + player.dir.y*move_speed) as usize][player.x as usize]);
+            if map[(player.y - player.dir.y*move_speed) as usize][player.x as usize] != "#" {player.y -= player.dir.y*move_speed};
+            if map[player.y as usize][(player.y - player.dir.y*move_speed) as usize] != "#" {player.x -= player.dir.x*move_speed};
         }
         if window.is_key_down(Key::A) {
-            if map[player.y as usize][player.x as usize] != "" {player.x += 0.1};
+            if map[player.y as usize][player.x as usize] != "#" {player.x += 0.1};
         }
         if window.is_key_down(Key::D) {
-            if map[player.y as usize][player.x as usize] != "" {player.x -= 0.1};
+            if map[player.y as usize][player.x as usize] != "#" {player.x -= 0.1};
         }
         if window.is_key_down(Key::Right){
             let old_plane = plane;
@@ -118,51 +130,50 @@ fn main() {
         // println!("{:?}", player);
 
         for x in 0..D_WIDTH{
-            let cam_x = 2.0*x as f32/ (D_WIDTH as f32)-1.0;
+            let cam_x = (2.0 * x as f32) / (D_WIDTH as f32 - 1.0);
             let ray = Ray {
                 dir_x : player.dir.x + plane.x * cam_x,
                 dir_y : player.dir.y + plane.y * cam_x
             };
-            let mut wall_hit = false;
-            // let mut current_point = PosVec {
+            // let mut current_point = FloatVec {
             //     x: player.x,
             //     y: player.y
             // };
             let mut side = false;
-            let mut map_vec = PosVec {
-                x: player.x,
-                y: player.y
+            let mut map_vec = IntVec {
+                x: player.x as i32,
+                y: player.y as i32
             };
-            let mut step = PosVec {
-                x : 0.0,
-                y : 0.0
+            let mut step = IntVec {
+                x : 0,
+                y : 0
             };
-            let mut delta_dist = PosVec {
+            let mut delta_dist = FloatVec {
                 x : 1.0/ray.dir_x,
                 y : 1.0/ray.dir_y
             };
-            let mut side_dist = PosVec {
+            let mut side_dist = FloatVec {
                 x : 0.0,
                 y : 0.0
             };
-            if ray.dir_x < 0.0 {
-                step.x = -1.0;
-                side_dist.x = (player.x - map_vec.x.round()) * delta_dist.x;
+            if ray.dir_x > -0.0_f32{
+                step.x = 1;
+                side_dist.x = (map_vec.x as f32 + 1.0 - player.x) * delta_dist.x;
             }else{
-                step.x = 1.0;
-                side_dist.x = (map_vec.x.round() + 1.0 - player.x) * delta_dist.x;
+                step.x = -1;
+                side_dist.x = (player.x - map_vec.x as f32) * delta_dist.x;
             }
-            if ray.dir_y < 0.0 {
-                step.y = -1.0;
-                side_dist.y = (player.y - map_vec.y.round()) * delta_dist.y;
-
+            if ray.dir_y > -0.0_f32 {
+                step.x = 1;
+                side_dist.y = (map_vec.y as f32 + 1.0 - player.y) * delta_dist.y;
             }else{
-                step.x = 1.0;
-                side_dist.y = (map_vec.y.round() + 1.0 - player.y) * delta_dist.y;
+                step.y = -1;
+                side_dist.y = (player.y - map_vec.y as f32) * delta_dist.y;
+                
 
             }
             let mut hit = false;
-            loop {
+            while !hit {
 
                 if side_dist.x < side_dist.y {
                     side_dist.x += delta_dist.x;
@@ -173,61 +184,54 @@ fn main() {
                     map_vec.y += step.y;
                     side = false;
                 }
-                println!("{}", x);
-                if map[map_vec.y as usize][map_vec.x as usize] == "#" {
-                    let distance = (map_vec.x - player.x).abs() + (map_vec.y - player.y).abs();
-                    let mut line_height = D_HEIGHT as f32/(distance as f32);
-                    if line_height < 0.0{
-                        line_height = 0.0;
-                    }else if line_height > D_HEIGHT as f32 {
-                        line_height = D_HEIGHT as f32 - 1.0
-                    }
-                    let start_pixel = -line_height/2.0 + D_HEIGHT as f32 /2.0;
-                    println!("{} {}", distance, line_height as usize);
-                    for i in 0..line_height as usize {
-                        if line_height > 0.0 {
-                            let pixel_to_draw = (start_pixel+i as f32)*D_WIDTH as f32 + x as f32;
-                            if pixel_to_draw > -0.0 && pixel_to_draw < (D_HEIGHT*D_WIDTH) as f32{
-                                buffer[pixel_to_draw as usize] = 255255255;
-                            }
+
+                // println!("{:?}", map_vec);
+            
+                if map[map_vec.x as usize][map_vec.y as usize] == "#" { 
+                    hit = true;
+                    // println!("{:?}", map_vec);
+                }
+            }
+                // let distance = side_dist.y.abs() + side_dist.x.abs();
+                let distance;
+                if !side {distance =(map_vec.x as f32 - player.x + (1-step.x) as f32 / 2.0) /ray.dir_x}
+                else {distance = (map_vec.y as f32 - player.y + (1-step.y) as f32 /2.0) /ray.dir_y}
+            
+                let mut line_height: i32;
+                let mut start_pixel;
+                // if side == false { distance = side_dist.x}
+                // else {distance = side_dist.y}
+                if distance > 0_f32 {
+                    line_height = (D_HEIGHT as f32/distance) as i32;
+                    
+                }else {
+                    line_height = 1;
+                    start_pixel = 0;
+                }
+                if line_height > -1 && line_height
+                 < (D_HEIGHT as i32){
+                    start_pixel = -line_height/2 + (D_HEIGHT as i32/2);
+                }else{
+                    start_pixel = 0;
+                    line_height = 0;
+                }
+                
+                
+                // println!("{} {} {} {:?} {:?} {:?}", distance, line_height, start_pixel as usize, player, map_vec, ray);
+                for i in 0..line_height as usize {
+                    if line_height as usize > 0 {
+                        let pixel_to_draw = (start_pixel as usize+i)*(D_WIDTH) + x ;
+                        if pixel_to_draw > 0 && pixel_to_draw < (D_HEIGHT*D_WIDTH){
+                            // println!("{}", pixel_to_draw);
+                            buffer[pixel_to_draw as usize] = 255;
                         }
                     }
-                    break;
-                    // hit = true;
                 }
-
-
-
-
-                // current_point.x += 0.010/ray.dir_x;
-                // current_point.y += 0.010/ray.dir_y;
-
-                // let distance = (current_point.x-player.x).abs() + (current_point.y-player.y).abs();
-                // if current_point.x > map[0].len() as f32 || current_point.y > map.len() as f32 
-                // || current_point.x < -0.0000 || current_point.y < -0.0000 {
-                //     break;
-                // }
-                // if map[current_point.y as usize][current_point.x as usize] == "#" {
-                //     let mut line_height = (1.0/distance)*D_HEIGHT as f32;
-                //     if line_height < 0.0{
-                //         line_height = 0.0;
-                //     }
-                //     let start_pixel = -line_height/2.0 + D_HEIGHT as f32 /2.0;
-                //     println!("{} {}", line_height, x);
-                //     for i in 0..line_height as usize {
-                //         if line_height > 0.0 {
-                //             let pixel_to_draw = (start_pixel+i as f32)*D_WIDTH as f32 + x as f32;
-                //             if pixel_to_draw > -0.0 && pixel_to_draw < (D_HEIGHT*D_WIDTH) as f32{
-                //                 buffer[pixel_to_draw as usize] = 255;
-                //             }
-                //         }
-                //     }
-                //     hit = true;
                 
                 
             }
             // println!("{} {:?} {:?}", cam_x, ray, current_point);
-        }
+        
         
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
